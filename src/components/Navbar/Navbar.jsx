92044d4398c6b2731ba27e35c9a9a04e7424afcb
Junh Saint Sanchez <saintjunh@gmail.com>
2026-08-31 12:52:50 +0800

    Initialize web application

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Navbar() {
+  return (
+    <nav style={{padding: '10px', background: '#222', color: '#fff'}}>
+      <strong>G-FLEET</strong>
+    </nav>
+  );
+}
